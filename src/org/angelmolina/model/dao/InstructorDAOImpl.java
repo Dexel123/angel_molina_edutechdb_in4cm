900c94ce4c5a2b9f8dbc11e9ef4447c5cbbde93a
Angel Molina <amolina-202603@kinal.edu.gt>
2026-05-28 23:09:44 -0600

    006 se agrego el DAOImpl

--- src/org/angelmolina/model/dao/InstructorDAOImpl.java
+++ src/org/angelmolina/model/dao/InstructorDAOImpl.java
@@ -1,13 +1,62 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package org.angelmolina.model.dao;
 
-/**
- *
- * @author Usuario
- */
-public class InstructorDAOImpl {
+import java.util.ArrayList;
+import java.util.List;
+import org.angelmolina.model.Instructor;
+import java.sql.Connection;
+import org.angelmolina.model.conexion.Conexion;
+import java.sql.CallableStatement;
+import java.sql.ResultSet;
+
+public class InstructorDAOImpl implements InstructorDAO {
+
+    @Override
+    public boolean instertar(Instructor instructor) {
+        return false;
+    }
+
+   @Override
+public List<Instructor> listar() {
+    List<Instructor> instructores = new ArrayList<>();
+    
+    // CORRECCIÓN: Quitamos el '(' inicial y el '}' final. Dejamos la sintaxis limpia de MySQL.
+    String consulta = "CALL sp_listar_instructores()"; 
+    
+    try (Connection conexion = Conexion.conectar();
+         CallableStatement call = conexion.prepareCall(consulta);
+         ResultSet resultado = call.executeQuery()) {
+         
+        while(resultado.next()){
+            instructores.add(new Instructor(
+                    resultado.getInt("dni_instructor"), // Asegúrate de que en tu BD la columna sea 'in_categoria' (o tal vez 'id_categoria')
+                    resultado.getString("nombre_isntructor"),
+                    resultado.getString("especialidad_isntructor")
+            ));
+        }
+        
+    } catch(Exception e) {
+        System.out.println("ERROR: al listar categorias: " + e.getMessage());
+        e.printStackTrace(); // Te agregué esto temporalmente por si surge algún otro detalle interno
+    }
+    return instructores;
+    }
+
+    @Override
+    public Instructor buscar(int dni) {
+        return null;
+    }
+
+    @Override
+    public boolean actualizar(Instructor instructor) {
+        return false;
+    }
+
+    @Override
+    public boolean eliminar(int dni) {
+         return false;
+    }
+    
     
 }
+
